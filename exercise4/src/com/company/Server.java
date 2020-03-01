6adeb0a9fd3d5d7deb7e9d38b80a276ac561a0b8
ericy <ericy@stud.ntnu.no>
2020-03-01 14:41:39 +0100

    finished with socket assignment

--- exercise4/src/com/company/Server.java
+++ exercise4/src/com/company/Server.java
@@ -3,90 +3,29 @@ package com.company;
 import java.io.*;
 import java.net.*;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 
 class Server{
 
-    enum Operations {
-        ADD,
-        SUBTRACT
-    }
+
 
     public static void main(String[]args) throws IOException {
-
-
 
         final int PORT = 7000;
         ServerSocket ss = new ServerSocket(PORT);
         System.out.println("Server is running on port " + PORT);
-        Socket connection = ss.accept(); //Maybe thread this?
+        ArrayList<ConnectionHandler> connections = new ArrayList<>();
+        Socket connection = null;
 
-
-        InputStreamReader readConnection = new InputStreamReader(connection.getInputStream());
-        BufferedReader reader = new BufferedReader(readConnection);
-        PrintWriter writer = new PrintWriter(connection.getOutputStream(), true);
-
-
-        writer.println("You are connected to the socket server");
-
-        String msg = reader.readLine();
-
-
-        while(msg != null){
-           String[] result = msg.split(" ");
-           Operations op = null;
-           try{
-               op = Operations.valueOf(result[0].toUpperCase());
-           } catch (IllegalArgumentException e){
-               writer.println(e);
-           }
-
-
-           switch (op){
-               case ADD:
-                   System.out.println("add");
-                   try{
-                       int no1 = Integer.parseInt(result[1]);
-                       int no2 = Integer.parseInt(result[2]);
-                       int resultOfOperation = addNumbers(no1, no2);
-                       writer.println(resultOfOperation);
-                   } catch (NumberFormatException e){
-                       writer.println(e);
-                   }
-                   break;
-
-               case SUBTRACT:
-                   System.out.println("subtract");
-                   try{
-                       int no1 = Integer.parseInt(result[1]);
-                       int no2 = Integer.parseInt(result[2]);
-                       int resultOfOperation = subtractNumbers(no1,no2);
-                       writer.println(resultOfOperation);
-                   } catch(NumberFormatException e){
-                       writer.println(e);
-                   }
-                   break;
-
-           }
-
-
-           msg = reader.readLine();
-
+        while (true) {
+            connection = ss.accept();
+            ConnectionHandler t = new ConnectionHandler(connection);
+            t.start();
+            connections.add(t); // Don't really need this, but have connections in reference if needed later on.
         }
 
 
-
-
-        connection.close();
-
-    }
-
-    public static int addNumbers(int no1, int no2){
-        return no1+no2;
-    }
-
-    public static int subtractNumbers(int no1, int no2){
-        return no1-no2;
     }
 
 }
